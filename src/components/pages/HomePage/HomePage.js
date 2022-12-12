52ee2eca515094fc20f2f0245003001152bde585
Lucas Pereira <lpmelo10@gmail.com>
2022-12-12 19:15:43 -0300

    feat: adding a segment to HomePage

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -11,21 +11,23 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <Grid stackable columns={2}>
-      <Grid.Row columns={1}>
-        <GridColumn textAlign="left">
-          <Container fluid>
-            <Header as="h2">Desafio Unicad - Desenvolvedor Fullstack</Header>
-            <p>
-              O desafio consiste em desenvolver um sistema que será a versão
-              mais simples possível de um sistema de entregas de mercadorias a
-              clientes. Ele deve possuir um cadastro de entrega, visualização de
-              entregas cadastradas e o percurso no mapa.
-            </p>
-          </Container>
-        </GridColumn>
-      </Grid.Row>
-    </Grid>
+    <Segment>
+      <Grid stackable columns={2}>
+        <Grid.Row columns={1}>
+          <GridColumn textAlign="left">
+            <Container fluid>
+              <Header as="h2">Desafio Unicad - Desenvolvedor Fullstack</Header>
+              <p>
+                O desafio consiste em desenvolver um sistema que será a versão
+                mais simples possível de um sistema de entregas de mercadorias a
+                clientes. Ele deve possuir um cadastro de entrega, visualização
+                de entregas cadastradas e o percurso no mapa.
+              </p>
+            </Container>
+          </GridColumn>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   );
 };
 
